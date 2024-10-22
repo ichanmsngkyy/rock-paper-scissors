baeaa789e3af4d6a88bfce11930cd939a5b2c66b
ichanmsngkyy <ichanmsngky@gmail.com>
2024-10-22 21:28:25 +0800

    Finished making Rock,Paper,Scissor game
    -Designed the structure and layout of the game
    -Implements all the functionalities needed in the game

--- script.js
+++ script.js
@@ -15,29 +15,66 @@ function getComputerChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
 
 const humanScoreSpan = document.querySelector('.humanScore');
 const computerScoreSpan = document.querySelector('.computerScore');
 const resultsDiv = document.querySelector('.results');
+const roundsPlayed = document.querySelector('.rounds');
+const restartGame = document.querySelector('.restartGame');
 
 function updateScore() {
     humanScoreSpan.textContent = humanScore;
     computerScoreSpan.textContent = computerScore;
+    roundsPlayed.textContent = rounds;
 }
 
 function checkWinner(){
     if(humanScore === 5){
         resultsDiv.textContent = "You Won the game!";
-        disableButtons();
+        showRestartButton();
     } else if (computerScore === 5){
         resultsDiv.textContent = "Computer won the game!";
+        showRestartButton();
+    } else if (rounds === 5){
+        if(humanScore > computerScore){
+            resultsDiv.textContent = "You Won the game!";
+        } else if (computerScore > humanScore){
+            resultsDiv.textContent = "Computer won the game!";
+        } else if (humanScore === computerScore){
+        resultsDiv.textContent = "The game is draw!";
+        }
         disableButtons();
+        showRestartButton();
     }
+}
+
+function showRestartButton(){
+    restartGame.style.display ="block";
+}
+
+function hideRestartButton(){
+    restartGame.style.display = "none";
 }
 
 function disableButtons() {
     const buttons = document.querySelectorAll('.buttonList button');
     buttons.forEach(button => button.disabled = true);
+}
+
+function resetGame(){
+    humanScore = 0;
+    computerScore = 0;
+    rounds = 0;
+    updateScore();
+    resultsDiv.textContent="";
+    hideRestartButton;
+    enableButtons();
+}
+
+function enableButtons(){
+    const buttons = document.querySelectorAll('.buttonList button');
+        buttons.forEach(button => button.disabled = false);
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -54,10 +91,11 @@ function playRound(humanChoice, computerChoice) {
     resultMessage = `You win ${humanChoice} beats ${computerChoice}.`;
    } else {
     computerScore++;
-    resultMessage = `You win ${computerChoice} beats ${humanChoice}.`;
+    resultMessage = `You lose ${computerChoice} beats ${humanChoice}.`;
    }
 
    resultsDiv.textContent = resultMessage;
+   rounds++;
    updateScore();
    checkWinner();
 }
@@ -67,6 +105,8 @@ const buttonList = document.querySelector('.buttonList');
 
 buttonList.addEventListener('click', (event) => {
     const humanChoice = event.target.className;
-    const computerChoice = getComputerChoice;
-    playRound(humanChoice, getComputerChoice);
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
 });
+
+restartGame.addEventListener('click', resetGame);
